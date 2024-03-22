82ea3b9a3d7ecb372dac1c68ef1916d2baf05bd6
Helsa Sriprameswari Putri <144691463+helsasp@users.noreply.github.com>
2024-03-22 16:10:07 +0700

    Update isabel.sh

--- task-4/isabel.sh
+++ task-4/isabel.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
+#4D
 download_fotonya_levi() {
-
 local tgl=$(date +"%Y%m%d")
-wget -O "levi_${tgl}.jpg" "https://www.sotwe.com/CaptainLeviCF"
-
+wget -O "levi_${tgl}.jpg" "https://kincir.com/movie/anime/attack-on-titan-alasan-eren-jadi-villain-v3owrltqlaip/"
 }
 
 download_fotonya_eren() {
-
 local tgl=$(date +"%Y%m%d")
 wget -O "eren_${tgl}.jpg" "https://kincir.com/movie/anime/attack-on-titan-alasan-eren-jadi-villain-v3owrltqlaip/"
-
 }
 
-download_gantian() {
-
-local tanggal_skg=$(date +"%j" | sed 's/^0*//')
-
+ tanggal_skg=$(date +"%j" | sed 's/^0*//')
  if [ $tanggal_skg -ge 1 ] && [ $tanggal_skg -le 365 ]; then
   if [ $((10#$tanggal_skg % 2)) -eq 0 ]; then
      download_fotonya_levi
@@ -28,6 +22,3 @@ local tanggal_skg=$(date +"%j" | sed 's/^0*//')
      echo "Tanggalnya salah!" >&2
        exit 1
  fi
-}
-
-download_gantian
