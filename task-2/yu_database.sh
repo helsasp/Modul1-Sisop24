95ee02432c8ad09ee9a07c2add9bc45711d16f18
Yosua Hares <104217128+yosuahres@users.noreply.github.com>
2024-03-22 16:03:36 +0700

    Update yu_database.sh

--- task-2/yu_database.sh
+++ task-2/yu_database.sh
@@ -2,10 +2,10 @@
 
 while IFS=',' read -r No Nama_Pengusul Departemen Fakultas Judul Pendamping Skema
 do
-    username="$Nama_Pengusul"
+    usrnme="$Nama_Pengusul"
 
-    password=$(echo "$Pendamping" | grep -o '[0-9]*')
-    password="$Fakultas$password"
+    temp=$(echo "$Pendamping" | grep -o '[0-9]*')
+    pass="$Fakultas$temp"
 
-    echo "$username:$password" >> users.txt
+    echo "$usrnme:$pass" >> users.txt
 done < /home/liares/Documents/sisop/pratikum1/praktikum-modul-1-d21/resources/data-pkm.csv
