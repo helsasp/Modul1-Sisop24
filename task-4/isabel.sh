bf7eb70d70bd726cb1d728c81b5f55603f6a4c3f
Yosua Hares <104217128+yosuahres@users.noreply.github.com>
2024-03-22 16:05:19 +0700

    Update isabel.sh

--- task-4/isabel.sh
+++ task-4/isabel.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+download_fotonya_levi() {
+
+local tgl=$(date +"%Y%m%d")
+wget -O "levi_${tgl}.jpg" "https://www.sotwe.com/CaptainLeviCF"
+
+}
+
+download_fotonya_eren() {
+
+local tgl=$(date +"%Y%m%d")
+wget -O "eren_${tgl}.jpg" "https://kincir.com/movie/anime/attack-on-titan-alasan-eren-jadi-villain-v3owrltqlaip/"
+
+}
+
+download_gantian() {
+
+local tanggal_skg=$(date +"%j" | sed 's/^0*//')
+
+ if [ $tanggal_skg -ge 1 ] && [ $tanggal_skg -le 365 ]; then
+  if [ $((10#$tanggal_skg % 2)) -eq 0 ]; then
+     download_fotonya_levi
+  else
+     download_fotonya_eren
+   fi
+ else
+     echo "Tanggalnya salah!" >&2
+       exit 1
+ fi
+}
+
+download_gantian
